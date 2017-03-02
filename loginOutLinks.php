f83802494b36560c2c87cde0aca6a9fc270f3650
A.J. de Mooij <A.J.deMooij@uu.nl>
2017-03-02 13:51:53 +0100

    Added example of production for logout link

--- loginOutLinks.php
+++ loginOutLinks.php
@@ -2,8 +2,8 @@
 require_once('functions.inc');
 
 if($as->isAuthenticated()) {
-	// $url = $as->getLogoutUrl('http://anamidp.services.uu.nl/nidp/logout');
-	$url = $as->getLogoutUrl('http://logout.acc.uu.nl');
+	$url = $as->getLogoutUrl('http://logout.acc.uu.nl'); // Acceptation
+	// $url = $as->getLogoutUrl('http://logout.uu.nl'); // Production
 	echo "You are logged in.";
 	echo "<br/>Your attributes are:";
 	echo prettyAttributes($as->getAttributes());
